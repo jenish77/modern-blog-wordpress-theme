5f5e4c074714c5f7d41e18519e1e622eb7132950
Jenish Maru <74286329+jenish77@users.noreply.github.com>
2026-01-12 23:23:45 +0530

    Social Share Options section in the Customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -33,5 +33,38 @@ function modern_blog_customize_register( $wp_customize ) {
         'label'    => __( 'Primary Theme Color', 'modern-blog-theme' ),
         'section'  => 'colors', // Core section
     ) ) );
+    // Add Social Share Options Section
+    $wp_customize->add_section( 'modern_blog_social_share_options', array(
+        'title'    => __( 'Social Share Options', 'modern-blog-theme' ),
+        'priority' => 135,
+    ) );
+
+    $social_platforms = array(
+        'facebook' => __( 'Facebook', 'modern-blog-theme' ),
+        'twitter'  => __( 'Twitter', 'modern-blog-theme' ),
+        'linkedin' => __( 'LinkedIn', 'modern-blog-theme' ),
+        'whatsapp' => __( 'WhatsApp', 'modern-blog-theme' ),
+    );
+
+    foreach ( $social_platforms as $id => $label ) {
+        $wp_customize->add_setting( 'modern_blog_share_' . $id, array(
+            'default'           => true,
+            'sanitize_callback' => 'modern_blog_sanitize_checkbox',
+            'transport'         => 'refresh',
+        ) );
+
+        $wp_customize->add_control( 'modern_blog_share_' . $id, array(
+            'label'    => sprintf( __( 'Enable %s Sharing', 'modern-blog-theme' ), $label ),
+            'section'  => 'modern_blog_social_share_options',
+            'type'     => 'checkbox',
+        ) );
+    }
 }
 add_action( 'customize_register', 'modern_blog_customize_register' );
+
+/**
+ * Sanitize checkbox input
+ */
+function modern_blog_sanitize_checkbox( $checked ) {
+    return ( ( isset( $checked ) && true == $checked ) ? true : false );
+}
